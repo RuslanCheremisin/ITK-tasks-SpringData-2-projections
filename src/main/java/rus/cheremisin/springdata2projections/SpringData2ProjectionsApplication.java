cb841dc6d6f9d9e69193ef940fc826449553e89b
rus <ruslan.cheremisin@gmail.com>
2026-04-22 01:01:50 +1000

    init commit

---
+++ src/main/java/rus/cheremisin/springdata2projections/SpringData2ProjectionsApplication.java
@@ -0,0 +1,13 @@
+package rus.cheremisin.springdata2projections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringData2ProjectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringData2ProjectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rus/cheremisin/springdata2projections/SpringData2ProjectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package rus.cheremisin.springdata2projections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringData2ProjectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
